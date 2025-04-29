31fc8ad55cd3416f7bf9aad616293dcda09c8e76
Muhammad Rifqi Alimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-04-29 18:32:00 +0700

    Membuat Procedure printdata

--- OOP.cpp
+++ OOP.cpp
@@ -6,7 +6,12 @@ class Mahasiswa{
     int nim;
     string nama;
     float nilai;
-    
+
+    void printData(){
+        cout << "Nim : " << nim <<endl;
+        cout << "Nama : " << nama <<endl;
+        cout << "Nilai : " << nilai <<endl;
+    }
 
 };
 
